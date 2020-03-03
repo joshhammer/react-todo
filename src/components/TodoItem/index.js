0f5e0ced48ff042dfb5d7de724cd3ea05a6f66a3
Josua Hämmerle <josua.haemmerle@ricardo.ch>
2020-03-03 15:54:20 +0100

    add custom checkbox and bug :)

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -13,12 +13,12 @@ const TodoItem = (props) => {
 
     return(
         <div className='list-element'>
-            <input type="checkbox"/>
-            <li 
-            className={ props.todo.completed ? 'completed-todo' : ''}
-            onClick={ toggleComplete }>
-            {props.todo.title}
-            </li>
+            <label className='container'>
+                <input type="checkbox" onClick={ toggleComplete } />
+                <span className='custom-checkbox'></span>
+
+            </label>
+            <li className={ props.todo.completed ? 'completed-todo' : ''}>{props.todo.title}</li>
             <button className='delete-btn' onClick={ removeTodo } >
                 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash" className="svg-inline--fa fa-trash fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"></path></svg>
             </button>
