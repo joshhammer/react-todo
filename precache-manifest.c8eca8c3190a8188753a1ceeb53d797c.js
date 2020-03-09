51622d18ffd2c7a12c97003ef897c666572314b2
Josua Hämmerle <josua.haemmerle@ricardo.ch>
2020-03-09 15:44:04 +0100

    Updates

---
+++ precache-manifest.c8eca8c3190a8188753a1ceeb53d797c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "775352bb1d08b834b6c4e3fa3ea88ff2",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "ccc541a5fd81079dd4e0",
+    "url": "/react-todo/static/css/main.08bcab08.chunk.css"
+  },
+  {
+    "revision": "b79540fd287542cc29c6",
+    "url": "/react-todo/static/js/2.ab12e582.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-todo/static/js/2.ab12e582.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccc541a5fd81079dd4e0",
+    "url": "/react-todo/static/js/main.b6b07991.chunk.js"
+  },
+  {
+    "revision": "6821c8abf30b581997d9",
+    "url": "/react-todo/static/js/runtime-main.d9f2c231.js"
+  }
+]);
